5159f2a1e0bb8314620563ec905f21157856bff1
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-04 03:32:35 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(array)
 end
 summon_captain_planet(["earth", "wind", "fire", "water", "heart"])
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)
   # Your code here
 end
 

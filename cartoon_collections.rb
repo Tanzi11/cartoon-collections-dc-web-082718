41b6f426c19b2d3db2c11f364303201124094b86
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2018-08-04 03:33:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ long_planeteer_calls(["puff", "go", "two"])
 
 def find_the_cheese(array)
  array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
+ cheese_types = ["cheddar", "gouda", "camembert"]
